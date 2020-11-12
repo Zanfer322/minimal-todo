c0476d10bab45484f9f8ca0e6b1d6f76c840ff0e
Zanfer322 <edupugantiarun@gmail.com>
2020-11-12 22:50:14 +0530

    add todo api

--- frontend/src/app/todo.service.ts
+++ frontend/src/app/todo.service.ts
@@ -1,9 +1,53 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Tag, Todo } from './models';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoService {
+  private cachedTags?: Tag[];
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.cachedTags = undefined;
+  }
+
+  async createTag(name: string): Promise<Tag> {
+    let data = await this.http.post('/api/tags/', { name }).toPromise();
+    let tag = this.toTag(data);
+    if (this.cachedTags) {
+      this.cachedTags.push(tag);
+    }
+    return tag;
+  }
+
+  async getAllTags(refresh: boolean = false): Promise<Tag[]> {
+    if (refresh == true && this.cachedTags != null) {
+      return this.cachedTags;
+    }
+    let dataArr = await this.http.get<any[]>('/api/tags/').toPromise();
+    let tags = dataArr.map((data) => this.toTag(data));
+    this.cachedTags = tags;
+    return tags;
+  }
+
+  private toTag(data: any): Tag {
+    return {
+      id: data.id,
+      name: data.name,
+      createdAt: new Date(data['created_at']),
+    };
+  }
+
+  private toTodo(data: any): Todo {
+    return {
+      id: data.id,
+      contents: data.contents,
+      state: data.state,
+      tags: data.tags,
+      createdAt: new Date(data['created_at']),
+      updatedAt: new Date(data['updated_at']),
+      stateUpdatedAt: new Date(data['state_updated_at']),
+    };
+  }
 }
